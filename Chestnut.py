3ad8afdf86ffd28011a01f7bcd1640aa9b83b2ab
Morris Frasier <morris.frasier@gmail.com>
2016-09-12 19:50:46 -0400

    Add files via upload

---
+++ Chestnut.py
@@ -0,0 +1,21 @@
+ValidInput=False
+while ValidInput==False:
+    try:
+        filename = input(" Please enter the path and name of the *.txt file: ")
+        f = open(filename, "r")
+        print(f.read(1))
+        print(f.read())
+        f.close()
+
+    except FileExistsError:
+        print("Sorry File Does Not Exist")
+    except PermissionError:
+        print(" Sorry You do Not have Permisison to Access the File")
+    except FileNotFoundError:
+        print(" Sorry file does not exist")
+
+    #f = open("test.txt", "r")
+    #myList = []
+    #for line in f:
+        #myList.append(line)
+    #print(myList)
